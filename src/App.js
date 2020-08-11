2fcab96db44048e8593c773b31e6286cb2874325
zooyangwoo <69509442+zooyangwoo@users.noreply.github.com>
2020-08-11 21:03:36 +0900

    1 day

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, Progress} from 'semantic-ui-css'
 function App() {
   return (
+    <div>
     <p> 안녕하세요 ! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
